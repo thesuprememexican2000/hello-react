2719e1790e395d3f66fabf45ac7ab455384c98e3
thesuprememexican2000 <darian321@outlook.com>
2023-05-01 14:24:29 -0400

    yes

---
+++ src/model/Task.js
@@ -0,0 +1,5 @@
+
+const mongoose = require('mongoose');
+const taskSchema = new mongoose.Schema({title: String, description: String, status: String});
+let Task = mongoose.model('Task', taskSchema);;
+module.exports = Task;
