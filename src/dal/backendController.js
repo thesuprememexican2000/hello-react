4505741bdf42b0cd036554648173fd75120e6d09
thesuprememexican2000 <darian321@outlook.com>
2023-05-01 14:24:09 -0400

    things and stuff

---
+++ src/dal/backendController.js
@@ -0,0 +1,13 @@
+const TaskDAO = require('../dal/TaskDAO');
+
+async function main() {
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log("Running backend controller main script: " + time);
+    TaskDAO.doConnect();
+
+    const tasks = await TaskDAO.getTasks();
+    console.log(tasks);
+
+    const found = 
+}
