bf273b7592baa42a13391c79129a5e2015947ce0
Toriku097 <ttramirez097@gmail.com>
2023-05-08 18:24:24 -0400

    Master

--- src/Route.js
+++ src/Route.js
@@ -3,24 +3,25 @@ import CreateTask from "./components/CreateTask";
 import GetTask from "./components/GetTask";
 import DeleteTask from "./components/DeleteTask";
 import UpdateTask from "./components/UpdateTask";
+import './css/style.css'
 
 const RouteComponent = () => (
     <>
     <nav>
-        <ul>
+        <ul id="menu">
             <li><Link to="/">Accueil</Link></li>
             <li><Link to="/create_task">Créer une tâche</Link></li>
             <li><Link to="/get_task">Afficher les tâches</Link></li>
         </ul>
     </nav>
     <Routes>
-        <Route exact path="/"                       element={<Home/>}/>
-        <Route path="/create_task"                  element={<CreateTask/>}/>
-        <Route path='/get_task'                     element={<GetTask/>}/>
-        <Route path='/get_task/delete'              element={<Delete/>}/>
-        <Route path='/get_task/delete/:id/:title'         element={<DeleteTask/>}/>
-        <Route path='/get_task/update'              element={<Update/>}/>
-        <Route path='/get_task/update/:id/:title/:desc/:status'    element={<UpdateTask/>}/>
+        <Route exact path="/"                                       element={<Home/>}/>
+        <Route path="/create_task"                                  element={<CreateTask/>}/>
+        <Route path='/get_task'                                     element={<GetTask/>}/>
+        <Route path='/get_task/delete'                              element={<Delete/>}/>
+        <Route path='/get_task/delete/:id/:title'                   element={<DeleteTask/>}/>
+        <Route path='/get_task/update'                              element={<Update/>}/>
+        <Route path='/get_task/update/:id/:title/:desc/:status'     element={<UpdateTask/>}/>
     </Routes>
     </>
 )
@@ -29,7 +30,7 @@ function Home() {
     return (
         <div>
             <h1>Application de liste des tâches</h1>
-            <p>App in progress...</p>
+            <p>Bienvenue</p>
         </div>
     )
 }
